edc0325d16b276fcda4993cdde36550d44ca70e4
nitin telang <nitintelang110@gmail.com>
2024-06-14 10:27:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
+import "./App.css";
+import Create from "./components/Create";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Read from "./components/Read";
+import Update from "./components/Update";
+
 function App() {
-  
   return (
-    <>
-     hello
-      
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Create />} />
+          <Route exact path="/read" element={<Read />} />
+          <Route exact path="/edit/:id" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
